42524ea4b1ebbf3645fc5550ebfee345c6c48b6c
Jane Smith <example@email.com>
2018-11-27 15:44:10 -0800

    warmup is done very basic tough

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+'use strict';
+
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1>Hello World</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
